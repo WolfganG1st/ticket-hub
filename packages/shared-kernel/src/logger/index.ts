f650a0d16f755bf532e40018192e131cc4f00c63
WolfgänG <felipefernandes.sjc@hotmail.com>
2025-12-31 06:06:01 -0300

    fix: Configure logger for test environments
    Silence logger output and disable `pino-pretty` transport in test environments for improved performance and reduced noise.

--- packages/shared-kernel/src/logger/index.ts
+++ packages/shared-kernel/src/logger/index.ts
@@ -2,16 +2,18 @@ import process from 'node:process';
 import pino from 'pino';
 
 const isDev = process.env.NODE_ENV !== 'production';
+const isTest = process.env.NODE_ENV === 'test';
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  ...(isDev && {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
+  level: process.env.LOG_LEVEL || (isTest ? 'silent' : 'info'),
+  ...(isDev &&
+    !isTest && {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+        },
       },
-    },
-  }),
+    }),
 });
